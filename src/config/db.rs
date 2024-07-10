131367b82c6d9fa87ca5f34b3505ed17044a00dc
fangweicong <fangweicong@pxb7.com>
2024-07-10 14:55:12 +0800

    save

--- src/config/db.rs
+++ src/config/db.rs
@@ -9,7 +9,7 @@ static DB_CLIENT: OnceCell<RBatis> = OnceCell::new();
 pub async fn init_db() -> &'static RBatis {
     let rb = RBatis::new();
     rb.link(MysqlDriver {}, &*env::var("DB_HOST").unwrap()).await.unwrap();
-    rb.get_pool().unwrap().set_max_open_conns(100).await;
+    rb.get_pool().unwrap().set_max_open_conns(1000).await;
 
     DB_CLIENT.set(rb).expect("Failed to set DB_CLIENT");
     DB_CLIENT.get().expect("DB_CLIENT is not initialized")
