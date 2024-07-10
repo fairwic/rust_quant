cf15c92a739518c00ed617bff3a7dda46767c015
fangweicong <fangweicong@pxb7.com>
2024-07-10 17:19:29 +0800

    save

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,8 @@ async fn main() -> anyhow::Result<()> {
     // let inst_ids = Arc::new(vec!["BTC-USDT-SWAP"]);
     // let times = Arc::new(vec!["1D"]);
 
-    let inst_ids = Arc::new(vec!["BTC-USDT-SWAP", "SOL-USDT-SWAP", "ETH-USDT-SWAP", "ADA-USDT-SWAP", "SUSHI-USDT-SWAP"]);
+    // let inst_ids = Arc::new(vec!["BTC-USDT-SWAP", "SOL-USDT-SWAP", "ETH-USDT-SWAP", "ADA-USDT-SWAP", "SUSHI-USDT-SWAP"]);
+    let inst_ids = Arc::new(vec!["BTC-USDT-SWAP", "ETH-USDT-SWAP"]);
     let times = Arc::new(vec!["4H", "1H", "5m", "1Dutc"]);
 
     // let inst_ids = Arc::new(vec!["BTC-USDT-SWAP", "SOL-USDT-SWAP", "ETH-USDT-SWAP"]);
