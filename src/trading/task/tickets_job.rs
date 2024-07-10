02d23a63d4134970878c367c410c3bfc672d93ba
fangweicong <fangweicong@pxb7.com>
2024-07-10 16:26:15 +0800

    save

--- src/trading/task/tickets_job.rs
+++ src/trading/task/tickets_job.rs
@@ -39,16 +39,28 @@ pub async fn init_all_ticker(inst_ids: &Arc<Vec<&str>>) -> anyhow::Result<()> {
         }
     };
 
-    //同步币币产品
-    let ins_type = "SPOT";
-    let ticker = Market::get_tickers(&ins_type, None, None).await?;
-    debug!("全部tickets: {:?}", ticker);
-
-    if ticker.len() > 0 {
-        let res = TicketsModel::new().await;
-        let res = res.add(ticker).await?;
-        debug!("插入数据库结果: {:?}", res);
-    }
+    // //同步币币产品
+    // let ins_type = "SPOT";
+    // let ticker = Market::get_tickers(&ins_type, None, None).await?;
+    // debug!("全部tickets: {:?}", ticker);
+    //
+    // if ticker.len() > 0 {
+    //       let model = TicketsModel::new().await;
+    //     for ticker in tickers {
+    //         //判断是否在inst_ids中
+    //         if inst_ids.contains(&&**&ticker.inst_id) {
+    //             //判断数据库是否有
+    //             let res = model.find_one(&ticker.inst_id).await?;
+    //             if res.len() > 0 {
+    //                 println!("已经存在,更新");
+    //                 let res = model.update(&ticker).await?;
+    //             } else {
+    //                 println!("不存在");
+    //                 let res = model.add(vec![ticker]).await?;
+    //             }
+    //         }
+    //     }
+    // }
     Ok(())
 }
 
